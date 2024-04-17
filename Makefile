94b8169cad2c2f55ae415f56404364e15a2a0bb4
Eric Schrock <eric.schrock22@gmail.com>
2024-04-16 21:18:54 -0400

    Password entry struct and methods

--- Makefile
+++ Makefile
@@ -11,6 +11,8 @@ test:
 	touch test.png test.jpg test.jpeg
 	./stegosaurus
 	./stegosaurus --save hi --find hi
+	./stegosaurus --save abcdefghijklmnopqrstu
+	./stegosaurus --find abcdefghijklmnopqrstu
 	./stegosaurus --save hi
 	./stegosaurus --find hi
 	rm test.png test.jpg test.jpeg
--- main.go
+++ main.go
@@ -32,6 +32,11 @@ func main() {
 		str = *findFlag
 	}
 
+	if len(str) > nameLen {
+		fmt.Println("Names cannot be larger than", nameLen, "bytes")
+		return
+	}
+
 	images := make([]string, 0)
 	for _, ext := range [3]string{"png", "jpg", "jpeg"} {
 		names, err := filepath.Glob("*." + ext)
@@ -59,13 +64,44 @@ func main() {
 }
 
 func savePassword(kek []byte, name string, images []string) {
-	fmt.Println("KEK:", kek)
-	fmt.Println("Save:", name)
-	fmt.Println("Images:", images)
+	//todo: find an image that does not already contain a password (for image in images, decrypt entry with kek and check magic)
+
+	var e entry
+	e.init(name)
+	e.generate_password()
+
+	fmt.Println(e) //todo: insert entry into image
 }
 
 func findPasswords(kek []byte, nameSubstring string, images []string) {
-	fmt.Println("KEK:", kek)
-	fmt.Println("Find:", nameSubstring)
-	fmt.Println("Images:", images)
+	//todo: for each image, decrypt entry, check magic, and check name contains nameSubstring
+	//todo: print matching name/password pairs
+}
+
+type entry struct {
+	magic    uint32
+	name     [nameLen]byte
+	password [20]byte
+}
+
+const magic = 0xDEADBEEF
+const nameLen = 20
+
+func (e *entry) init(name string) {
+	if len(name) > len(e.name) {
+		log.Fatal("Name too large")
+	}
+
+	e.magic = magic
+	copy(e.name[:], name)
+}
+
+func (e *entry) generate_password() {
+	copy(e.password[:], "password123") //todo: generate random password
+}
+
+func (e *entry) check_magic() {
+	if e.magic != magic {
+		log.Fatal("Invalid magic")
+	}
 }
